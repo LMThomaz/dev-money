f9ccd20f4877ebc924e659f9fe6a287e2f72e2df
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-17 20:37:50 -0300

    Configurando MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,30 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Footer',
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
