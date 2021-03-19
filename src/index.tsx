fc2ee92bb1be019c7d00dd8967e96aad4a8aee9d
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-19 20:56:14 -0300

    Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance do ClickQuote',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 4500,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Monitor 19"',
+          type: 'withdraw',
+          category: 'Setup',
+          amount: 1600,
+          createdAt: new Date('2021-01-11 09:00:00'),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
