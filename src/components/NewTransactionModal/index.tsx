8033284f21603e0493efec2cbe7e155d6d1e0a60
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-20 16:21:21 -0300

    Finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,7 +22,7 @@ export function NewTransactionModal({
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
     const newTransaction = {
@@ -32,7 +32,13 @@ export function NewTransactionModal({
       type,
     };
 
-    createTransaction(newTransaction);
+    await createTransaction(newTransaction);
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
   }
 
   return (
