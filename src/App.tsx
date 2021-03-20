593d3db1bd38afec8f6c6c3978dff6ce0e1b4982
Leonardo Thomaz <33641830+LMThomaz@users.noreply.github.com>
2021-03-20 16:38:51 -0300

    Criando hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import Modal from "react-modal";
-import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header";
-import { NewTransactionModal } from "./components/NewTransactionModal";
-import { GlobalStyle } from "./styles/global";
-import { TransactionsProvider } from "./TransactionsContext";
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './hooks/useTransactions';
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
